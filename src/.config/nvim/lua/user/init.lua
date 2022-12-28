c7fb8ff9fcb75b0d80b69ab21e9edd06705c9cdd
Sebastian LaVine <mail@smlavine.com>
2022-12-28 13:17:49 -0500

    nvim: Move which-key config closer to key mappings

--- src/.config/nvim/lua/user/init.lua
+++ src/.config/nvim/lua/user/init.lua
@@ -227,6 +227,22 @@ local config = {
     },
   },
 
+  -- Modify which-key registration (Use this with mappings table in the above.)
+  ["which-key"] = {
+    -- Add bindings which show up as group name
+    register = {
+      -- first key is the mode, n == normal mode
+      n = {
+        -- second key is the prefix, <leader> prefixes
+        ["<leader>"] = {
+          -- third key is the key to bring up next level and its displayed
+          -- group name in which-key top level menu
+          ["b"] = { name = "Buffer" },
+        },
+      },
+    },
+  },
+
   -- Configure plugins
   plugins = {
     init = {
@@ -282,22 +298,6 @@ local config = {
       luasnip = 750,
       buffer = 500,
       path = 250,
-    },
-  },
-
-  -- Modify which-key registration (Use this with mappings table in the above.)
-  ["which-key"] = {
-    -- Add bindings which show up as group name
-    register = {
-      -- first key is the mode, n == normal mode
-      n = {
-        -- second key is the prefix, <leader> prefixes
-        ["<leader>"] = {
-          -- third key is the key to bring up next level and its displayed
-          -- group name in which-key top level menu
-          ["b"] = { name = "Buffer" },
-        },
-      },
     },
   },
 
