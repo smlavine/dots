6d167529a2e7e7ec51f0d0a0305015d1672860c5
Sebastian LaVine <mail@smlavine.com>
2022-12-27 23:57:54 -0500

    Source Vimscript with ToggleColumnWidth

--- src/.config/nvim/lua/user/init.lua
+++ src/.config/nvim/lua/user/init.lua
@@ -368,18 +368,7 @@ local config = {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
-    -- Set up custom filetypes
-    -- vim.filetype.add {
-    --   extension = {
-    --     foo = "fooscript",
-    --   },
-    --   filename = {
-    --     ["Foofile"] = "fooscript",
-    --   },
-    --   pattern = {
-    --     ["~/%.config/foo/.*"] = "fooscript",
-    --   },
-    -- }
+    vim.cmd 'source ~/.config/nvim/lua/user/init.vim'
   end,
 }
 
