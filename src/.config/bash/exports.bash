6ccfeac0c4cb82c42d031235eb7e44cb32df8582
Sebastian LaVine <mail@smlavine.com>
2025-01-27 14:01:49 -0500

    exports.bash: Clarify PS1 comment

--- src/.config/bash/exports.bash
+++ src/.config/bash/exports.bash
@@ -11,7 +11,7 @@ export PAGER="less"
 # Ex: 'master$ '
 export PS1="\[\033[01;35m\]\$(git symbolic-ref --short HEAD 2>&-)\[\033[01;32m\]$\[\033[00m\] "
 
-# 'user@hostname:~$ ' but with some color in it
+# Alternate: 'user@hostname:~$ ' but with some color in it
 #export PS1='\[\033[01;32m\]\u\[\033[01;31m\]@\[\033[01;36m\]\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;33m\]$\[\033[00m\] '
 
 # Disable less history
